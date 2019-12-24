ef67c64b32939e8f388c1aa95fc8d3789d6a25d0
Monica Scroggs <scrmr0210@gmail.com>
2019-12-23 21:38:57 -0500

    products table showing in console when server runs

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,3 +1,6 @@
+// Load the NPM Package inquirer
+const inquirer = require("inquirer");
+
 var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -14,3 +17,38 @@ connection.connect(function (err) {
 
     console.log('connected as id ' + connection.threadId);
 });
+
+connection.query("SELECT item_id, product_name, price FROM products", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+});
+
+
+// inquirer.prompt([
+
+//     {
+//         type: "list",
+//         name: "allProducts",
+//         choices: ["I made you cookies!", "No lie dude. I'm here to rob you.", "Uh. This is my house... Who are YOU???"]
+//     },
+
+// ]).then(function (user) {
+
+//         // If the user guesses the password...
+//         if (user.myPassword === "myHouse") {
+
+//             console.log("==============================================");
+
+//         }
+
+
+//         // If the user doesn't guess the password...
+//         else {
+
+//             console.log("othert thing=======");
+//         }
+//     });
+
+
+// need to move on with instructions after creating server
+// include inquirer in here and make sure i npm install mysql, npm install inquirer
